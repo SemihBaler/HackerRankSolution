60f43d5be6b0f74eff6b795ac695267f90e30f75
Semih Baler <semihbaler@gmail.com>
2023-09-14 10:28:00 +0300

    Plus_Minus solution added

--- Mini-Max_Sum/Mini-Max_Sum/Program.cs
+++ Mini-Max_Sum/Mini-Max_Sum/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        int min = 0, max = 0, sum = 0;
+        min = arr[0];
+        for (int i = 0; i < arr.Count; i++)
+        {
+            sum += arr[i];
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        Console.WriteLine((sum - max) + " " + (sum - min));
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
