3971882ddbb8854bf305f97447fae582fe7d1401
Semih Baler <semihbaler@gmail.com>
2023-09-14 12:01:50 +0300

    Time_Conversion solution added

---
+++ Time_Conversion/Time_Conversion/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Time_Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
