fc8b7bb17e0ac8a0a9ebb838a146a9dd0cfe1edd
Semih Baler <semihbaler@gmail.com>
2023-09-17 13:04:17 +0300

    Number_Line_Jumps solution added

---
+++ Number_Line_Jumps/Number_Line_Jumps/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Number_Line_Jumps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
