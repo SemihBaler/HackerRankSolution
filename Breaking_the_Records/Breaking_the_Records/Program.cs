481b046fdad49707a78253b2327fcc99201222f2
Semih Baler <semihbaler@gmail.com>
2023-09-18 12:35:29 +0300

    Subarray_Division solution added

--- Breaking_the_Records/Breaking_the_Records/Program.cs
+++ Breaking_the_Records/Breaking_the_Records/Program.cs
@@ -1,10 +1,72 @@
-﻿namespace Breaking_the_Records
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
 {
-    internal class Program
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<int> breakingRecords(List<int> scores)
     {
-        static void Main(string[] args)
+        List<int> result = new List<int>();
+        int best = scores[0];
+        int worst = scores[0];
+        int bestCount = 0;
+        int worstCount = 0;
+        for (int i = 0; i < scores.Count - 1; i++)
         {
-            Console.WriteLine("Hello, World!");
+            if (best > scores[i + 1])
+            {
+                bestCount++;
+                best = scores[i + 1];
+
+            }
+            else if (worst < scores[i + 1])
+            {
+                worstCount++;
+                worst = scores[i + 1];
+            }
+
         }
+        result.Add(worstCount);
+        result.Add(bestCount);
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+        List<int> result = Result.breakingRecords(scores);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
---
+++ Subarray_Division/Subarray_Division/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Subarray_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 2;
+            int d = 3;
+            List<int> s = new List<int>()
+            {
+                1,2,1,3,2,3,0
+            };
+            int count = 0;
+
+            for (int i = 0; i < s.Count; i++)
+            {
+                int tmp = 0;
+
+                for (int j = i; j < i + m; j++)
+                {
+                    if (j == s.Count) break;
+                    tmp += s[j];
+                }
+
+                if (d == tmp) count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
