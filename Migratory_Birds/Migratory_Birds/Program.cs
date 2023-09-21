12d880122aec551a966902be9dac300f320cc8e9
Semih Baler <semihbaler@gmail.com>
2023-09-21 10:49:02 +0300

    Migratory_Birds solution added

---
+++ Migratory_Birds/Migratory_Birds/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Migratory_Birds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new List<int>()
+            {
+                1,2,5,6,78,11,45,1,2,6,4,4,4,2,2,5,5,7
+            };
+            int[] counts = new int[6];
+            foreach (var item in arr)
+            {
+                counts[item]++;
+            }
+
+            int maxCount = counts.Max();
+            int mostFrequentBird = Array.IndexOf(counts, maxCount);
+
+            Console.WriteLine(mostFrequentBird);
+        }
+    }
+}
