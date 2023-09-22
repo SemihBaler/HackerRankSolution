79b759e3010eeb901725c91a6ea920630fe63cf5
Semih Baler <semihbaler@gmail.com>
2023-09-22 09:55:37 +0300

    Bill_Division solution added

---
+++ Bill_Division/Bill_Division/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bill_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Day_of_the_Programmer/Day_of_the_Programmer/Program.cs
+++ Day_of_the_Programmer/Day_of_the_Programmer/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace Day_of_the_Programmer
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
 {
-    internal class Program
+
+    /*
+     * Complete the 'dayOfProgrammer' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER year as parameter.
+     */
+
+    public static string dayOfProgrammer(int year)
     {
-        static void Main(string[] args)
+        if (year == 1918)
         {
-            Console.WriteLine("Hello, World!");
+            return "26.09.1918";
         }
+        else if ((year < 1918 && year % 4 == 0) || // Julian takvimi
+                 (year > 1918 && ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))))
+        {
+            return "12.09." + year;
+        }
+        else
+        {
+            return "13.09." + year;
+        }
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int year = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = Result.dayOfProgrammer(year);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
