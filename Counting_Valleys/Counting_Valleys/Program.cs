68e6ea4a41142b76c272360458f2eee4f539a84f
Semih Baler <semihbaler@gmail.com>
2023-09-25 23:18:26 +0300

    Counting_Valleys solution added

---
+++ Counting_Valleys/Counting_Valleys/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Counting_Valleys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Sales_by_Match/Sales_by_Match/Program.cs
+++ Sales_by_Match/Sales_by_Match/Program.cs
@@ -1,39 +1,70 @@
-﻿namespace Sales_by_Match
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
 {
-    internal class Program
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
     {
-        static void Main(string[] args)
+        int count = 0;
+        int max = ar.Max() + 1;
+        for (int i = 0; i < n - 1; i++)
         {
-            var ar = new List<int>()
+            if (ar[i] == max)
             {
-                10,20,20,10,10,30,50,10,20,10
-            };
-            int n=ar.Count;
-            int count = 0;
-            int max = ar.Max() + 1;
-            for (int i = 0; i < n - 1; i++)
+                continue;
+            }
+            for (int x = i + 1; x < n; x++)
             {
-                if (ar[i] == max)
+                if (ar[i] == ar[x])
                 {
-                    continue;
-                }
-                for (int x = i + 1; x < n; x++)
-                {
-                    if (ar[i] == ar[x])
-                    {
-                        ar[i] = max;
-                        ar[x] = max;
-                        count++;
-                        break;
-                    }
+                    ar[i] = max;
+                    ar[x] = max;
+                    count++;
+                    break;
                 }
             }
-            Console.WriteLine(count);
-
-
-
-
-
         }
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
