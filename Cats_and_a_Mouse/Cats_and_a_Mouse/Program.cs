46328165e2f3354c30c12dba6972199c6592e2cb
Semih Baler <semihbaler@gmail.com>
2023-09-27 22:20:03 +0300

    Cats_and_a_Mouse solution added

---
+++ Cats_and_a_Mouse/Cats_and_a_Mouse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cats_and_a_Mouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Counting_Valleys/Counting_Valleys/Program.cs
+++ Counting_Valleys/Counting_Valleys/Program.cs
@@ -1,10 +1,69 @@
-﻿namespace Counting_Valleys
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
 {
-    internal class Program
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
     {
-        static void Main(string[] args)
+        int count = 0;
+        int level = 0;
+
+        for (int i = 0; i < steps; i++)
         {
-            Console.WriteLine("Hello, World!");
+
+            if (path[i] == 'D' && level == 0)
+            {
+                count++;
+                level--;
+                continue;
+            }
+
+
+            if (path[i] == 'D') level--;
+            else if (path[i] == 'U') level++;
         }
+        return count;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
