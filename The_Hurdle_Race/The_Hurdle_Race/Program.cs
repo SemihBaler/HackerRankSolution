ce69dfe14b897a343f630b54a1ad5cb0798cd0de
Semih Baler <semihbaler@gmail.com>
2023-09-29 14:08:47 +0300

    The_Hurdle_Race solution added

---
+++ The_Hurdle_Race/The_Hurdle_Race/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Hurdle_Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
