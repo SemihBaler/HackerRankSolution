2fcc1c51541e93feff153f1824f656d28cfbd57f
Semih Baler <semihbaler@gmail.com>
2023-10-01 14:58:09 +0300

    Designer_PDF_Viewer soltion added

---
+++ Designer_PDF_Viewer/Designer_PDF_Viewer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<int> h, string word)
+    {
+        int count = 0;
+        var number = new List<int>();
+        var value = new List<int>();
+        var alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < word.Length; i++)
+        {
+            foreach (var item in alphabet)
+            {
+                count++;
+                if (item == word[i])
+                {
+                    number.Add(count);
+                    count = 0;
+                    break;
+                }
+            }
+        }
+        for (int x = 0; x < number.Count; x++)
+        {
+            value.Add(h[number[x] - 1]);
+        }
+        return value.Max() * (value.Count);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+        string word = Console.ReadLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
