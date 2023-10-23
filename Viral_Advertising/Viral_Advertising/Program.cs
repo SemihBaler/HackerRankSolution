7ebb0fb3d0038ea62c5a7e3a7c083858d9e306ee
Semih Baler <semihbaler@gmail.com>
2023-10-23 22:01:51 +0300

    Viral_Advertising solution added

---
+++ Viral_Advertising/Viral_Advertising/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n)
+    {
+        double count = 5;
+        double like = 0;
+        double totalLike = 0;
+        for (int i = 0; i < n; i++)
+        {
+            like = Math.Floor(count / 2);
+            totalLike += like;
+            count = like * 3;
+        }
+        return Convert.ToInt32(totalLike);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.viralAdvertising(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
